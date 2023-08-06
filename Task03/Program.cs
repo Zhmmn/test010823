52d6c16a886bcb77b9de53ae6dccb1833ebf0b44
Zhoomart <jabdykasymov@gmail.com>
2023-08-06 20:16:09 +0600

    Dni nedeli

---
+++ Task03/Program.cs
@@ -0,0 +1,42 @@
+﻿﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+
+
+Console.WriteLine("Введите номер дня недели");
+string dayNumber = Console.ReadLine();
+
+if(dayNumber == "1")
+{
+Console.WriteLine("Понедельник");
+}
+
+else if(dayNumber == "2")
+{
+Console.WriteLine("Vtornik");
+}
+
+else if(dayNumber == "3")
+{
+Console.WriteLine("Sreda");
+}
+else if(dayNumber == "4")
+{
+Console.WriteLine("Chetverg");
+}
+
+else if(dayNumber == "5")
+{
+Console.WriteLine("Pyatnica");
+}
+else if(dayNumber == "6")
+{
+Console.WriteLine("Subbota");
+}
+else if(dayNumber == "7")
+{
+Console.WriteLine("Voskresenie");
+}
+
