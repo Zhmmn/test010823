f4929a44d077309f8cb970a1f61bc29ad2c0a556
Zhoomart <jabdykasymov@gmail.com>
2023-08-06 22:14:31 +0600

    Большее и меньшее значения

---
+++ Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Напишите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+if(firstNumber > secondNumber)
+{
+  Console.WriteLine($"Большее = {firstNumber} - Меньшее = {secondNumber}");
+}
+
+else
+{
+  Console.WriteLine($"Большее = {secondNumber} - Меньшее = {firstNumber}");
+}
