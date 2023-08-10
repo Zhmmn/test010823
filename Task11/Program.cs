a396ea83d3b28c894f1a71f30c45eb0ea254b0cf
Zhoomart <jabdykasymov@gmail.com>
2023-08-10 22:56:59 +0600

    2 цифры из трехзначного числа

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+// 456-> 46
+// 782-> 72
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($" Случайное число в диапазоне от 100 до 1000 -> {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine($" 2 цифры из трехзначного числа {number} -> {result}");
+
+
+
