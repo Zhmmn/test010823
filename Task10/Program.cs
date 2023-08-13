ea0ed12291bb536ddc6155c4b175e3894a085c7e
Zhoomart <jabdykasymov@gmail.com>
2023-08-13 20:32:25 +0600

    Вывод второй цифры из трехзначного числа

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,10 +3,6 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-
 int SecondDigitThirdNumber(int number)
 {
   int firstDigit = number % 100; // 456 % 100 = 4.56 = 56
@@ -14,6 +10,12 @@ int SecondDigitThirdNumber(int number)
   return result;
 
 }
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
 
 
 if (number < 100 || number > 1000)
