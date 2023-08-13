190a485d23ed012a45166423592523ce0e300cdc
Zhmmn <140652051+Zhmmn@users.noreply.github.com>
2023-08-13 20:30:23 +0600

    Update Program.cs
    Перенес метод на начало кода

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,24 +2,19 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-
-Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-
 int SecondDigitThirdNumber(int number)
 {
   int firstDigit = number % 100; // 456 % 100 = 4.56 = 56
   int result = firstDigit / 10; // 56 / 10 = 5
   return result;
-
 }
 
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 if (number < 100 || number > 1000)
 {
   Console.WriteLine("Число не соответствует условию, введите другое число");
-
 }
 else
 {
