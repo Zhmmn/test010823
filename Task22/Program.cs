00f6032a6d18ff153cb36ed63e6d835c13d8714f
Zhoomart <jabdykasymov@gmail.com>
2023-08-24 15:51:16 +0600

    Square

---
+++ Task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// 22.Создать программу, которая 
+// 1 принимает на вход число (N) и 
+// 2 выдает таблицу квадратов чисел от 1 до N.
+
+void tableSquare(int number) // void в переводе пустой, не возвращает значение
+{
+  int i = 1;
+
+  while (i <= number)
+  {
+    Console.WriteLine($"{i,3} в квадрате: {i * i,5}"); // 3 и 5 кол-во отступа для ровного выведения чисел
+    i++;
+  }
+}
+
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+tableSquare(number);
