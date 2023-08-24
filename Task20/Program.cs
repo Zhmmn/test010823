4f0c6e65f37746da236afe0401ff7fe11de9b794
Zhoomart <jabdykasymov@gmail.com>
2023-08-24 14:57:23 +0600

    комменты

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -2,19 +2,15 @@
 // А (3,6); B(2,1) -> 5,09
 // A (7, -5) B (1, -1) -> 7,21
 
-int Quarter(int x, int y)
+double find(int xA, int yA, int xB, int yB)
 {
-  if (x > 0 && y > 0) return 1;
-  if (x < 0 && y > 0) return 2;
-  if (x < 0 && y < 0) return 3;
-  if (x > 0 && y < 0) return 4;
-  return 0;
+  return Math.sgrt(xA, yA,)
 }
 
-double sgrt = Math.Sqrt(5);
-double pow = Math.Pow(5, 3);
+double sgrt = Math.Sqrt(5); // квадратный корень
+double pow = Math.Pow(5, 3); // возведение в степень
 
 double d = 5.098889; // 5.09
 double dRound = Math.Round(d, 2, MidpointRounding.ToZero);
-Console.WriteLine(dRound);
+Console.WriteLine(dRound); // округление
 Console.WriteLine($"{d:F2}");
