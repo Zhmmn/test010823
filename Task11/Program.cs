d930fef882a3e40549077bd91ba424bcaee435c3
Zhoomart <jabdykasymov@gmail.com>
2023-08-25 13:46:03 +0600

    Полиндром

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,23 +1,54 @@
-﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
-// 456-> 46
-// 782-> 72
+﻿using System;
 
-
-int number = new Random().Next(100, 1000);
-// int firstDigit = number / 100;
-// int thirdDigit = number % 10;
-// int result = firstDigit * 10 + thirdDigit;
-
-int digit(int num)
+public class Answer
 {
-  int firstDigit = num / 100;
-  int thirdDigit = num % 10;
-  int result = firstDigit * 10 + thirdDigit;
-  return result;
+  static bool IsPalindrome(int number)
+  {
+    // Введите свое решение ниже
+    if (number >= 10000 && number < 100000)
+    {
+      int firstDigit = number / 10000;
+      int fifthDigit = number % 10;
+      int secondDigit = (number / 1000) % 10;
+      int fourthDigit = (number / 10) % 10;
+
+
+      if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+
+    }
+
+    else
+    {
+      Console.WriteLine("Число не пятизначное");
+      return false;
+    }
+
+  }
+
+  // Не удаляйте и не меняйте метод Main! 
+  static public void Main(string[] args)
+  {
+    int number;
+
+    if (args.Length >= 1)
+    {
+      number = int.Parse(args[0]);
+    }
+    else
+    {
+      // Здесь вы можете поменять значения для отправки кода на Выполнение
+      number = 64548;
+    }
+
+    // Не удаляйте строки ниже
+    bool result = IsPalindrome(number);
+    System.Console.WriteLine($"{result}");
+  }
 }
-
-int solution = digit(number);
-Console.WriteLine($" 2 цифры из трехзначного числа {number} -> {solution}");
-
-
-
