957f056a1163ba1297058a64eb1a83d4081953f9
Zhoomart <jabdykasymov@gmail.com>
2023-09-01 00:11:13 +0600

    massiv 8 elementov

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -4,9 +4,34 @@
 // [1,0,1,0,0,1,0,1]
 
 
-int[] array = new int[8];
-for (int i = 1; i < array.Length; i++)
+// int[] array = new int[8];
+// for (int i = 1; i < array.Length; i++)
+// {
+//   array[i] = i * i;
+//   Console.Write(array[i] + " ");
+// }
+
+void FillArray(int[] massiv)
 {
-  array[i] = i * i;
-  Console.Write(array[i] + " ");
+  int i = 0;
+  while (i < massiv.Length)
+  {
+    massiv[i] = new Random().Next(2);
+    i++;
+  }
 }
+
+void printArray(int[] massiv)
+{
+  int i = 0;
+  while (i < massiv.Length)
+  {
+    Console.Write($"{massiv[i]} ");
+    i++;
+  }
+}
+
+int[] array = new int[8];
+FillArray(array);
+printArray(array);
+
